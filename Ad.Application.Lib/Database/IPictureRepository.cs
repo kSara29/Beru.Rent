501c3c76fb509daf6f6bd0805d4db1366ba78d8d
Tyurmina Iolanta <lanax94@mail.ru>
2023-12-13 21:22:54 +0300

    #5 добавила интерфейс для работы с репозиторием картинки

---
+++ Ad.Application.Lib/Database/IPictureRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Ad.Application.Lib.Services;
+
+public interface IPictureRepository
+{
+    public bool SavePictureAnync();
+    public bool DeletePictureAnync(Guid id);
+    public string GetPictureAsync(Guid id);
+}
---
+++ Ad.Infrastructure.Lib/Database/PictureRepository.cs
@@ -0,0 +1,21 @@
+﻿using Ad.Application.Lib.Services;
+
+namespace Ad.Infrastructure.Lib.Database;
+
+public class PictureRepository : IPictureRepository
+{
+    public bool SavePictureAnync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeletePictureAnync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetPictureAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
