8c51c77e305f1ba94b699f69b5b8f2f81a8901aa
Tyurmina Iolanta <lanax94@mail.ru>
2023-12-19 18:23:33 +0300

    #49 добавлены модули Auth и Ocelot GateWay

--- Beru.Rent/Ad.Infrastructure.Lib/Database/AdRepository.cs
+++ Beru.Rent/Ad.Infrastructure.Lib/Database/AdRepository.cs
@@ -9,4 +9,14 @@ public class AdRepository : IAdRepository
         Console.WriteLine("saving ad in db");
         throw new NotImplementedException();
     }
+
+    public Domain.Core.Models.Ad ArchiveAsync(Domain.Core.Models.Ad ad)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Domain.Core.Models.Ad ActivateAsync(Domain.Core.Models.Ad ad)
+    {
+        throw new NotImplementedException();
+    }
 }
