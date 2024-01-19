64c1d7c10ea1a06e8cf5201d02eeca43cd0fc894
Bauyrzhan Yerikov <bauka.erikov@gmail.com>
2024-01-19 17:13:38 +0600

    #71 Проверка загрузки массива дат

--- Beru.Rent/Services/Deal.Service/Deal.Api/Endpoints/GetAllBookings.cs
+++ Beru.Rent/Services/Deal.Service/Deal.Api/Endpoints/GetAllBookings.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices.JavaScript;
 using Deal.Api.DTO.Booking;
 using Deal.Application.Contracts.Booking;
 using Deal.Application.DTO.Booking;
@@ -15,6 +16,22 @@ public class GetAllBookings(IBookingService service): Endpoint<GetAllBookingDto>
     public override async Task HandleAsync(GetAllBookingDto model, CancellationToken ct)
     {
         var results = await service.GetAllBookingsAsync(model.AdId);
-        await SendAsync(results, cancellation: ct);
+        DateArray[] dateTimes = new DateArray[results.Length];
+        for (int i = 0; i < results.Length; i++)
+        {
+            dateTimes[i].from = results[i,0];
+            dateTimes[i].to = results[i,1];
+        }
+        /*
+           [[дата начала, дата конца],
+            [дата начала, дата конца],
+            [дата начала, дата конца]]
+
+            [[[дата начала],[дата конца]],
+             [[дата начала],[дата конца]],
+             [[дата начала],[дата конца]]]
+
+           */
+        await SendAsync(dateTimes, cancellation: ct);
     }
 }
---
+++ Beru.Rent/Services/Deal.Service/Deal.Application/DTO/Booking/DateArray.cs
@@ -0,0 +1,11 @@
+using System.Runtime.InteropServices.JavaScript;
+
+namespace Deal.Application.DTO.Booking;
+
+public class DateArray
+{
+    public DateTime from { get; set; }
+    public DateTime to { get; set; }
+    
+    
+}
