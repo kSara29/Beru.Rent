df99f5066cc8be2167623aedf1af3ec470988d26
Tyurmina Iolanta <lanax94@mail.ru>
2024-01-28 13:49:40 +0300

    #79 добавила сервис для работы с Ad Service в Bff

---
+++ Beru.Rent/Api.Gateway/Bff.Api/Endpoints/AdService/CreateAd.cs
@@ -0,0 +1,23 @@
+﻿using Ad.Dto.CreateDtos;
+using Bff.Application.Contracts;
+using Common;
+using FastEndpoints;
+namespace Bff.Api.Endpoints.AdService;
+
+
+public class CreateAd(IAdService service) : Endpoint<CreateAdDto, ResponseModel<Guid>>
+{
+    public override void Configure()
+    {
+        Post("/bff/user/createUser");
+        AllowAnonymous();
+    }
+    
+    public override async Task HandleAsync
+        (CreateAdDto? request, CancellationToken ct)
+    { 
+        if (request is null) await SendAsync(null!, cancellation: ct);
+        var response = await service.CreateAdAsync()
+        await SendAsync(response, cancellation: ct);
+    }
+}
--- Beru.Rent/Api.Gateway/Bff.Application/Contracts/IAdService.cs
+++ Beru.Rent/Api.Gateway/Bff.Application/Contracts/IAdService.cs
@@ -4,4 +4,59 @@ namespace Bff.Application.Contracts;
 
 public interface IAdService
 {
+    #region методы объявления
+
+    Task<ResponseModel<Guid>> CreateAdAsync(CreateAdDto ad);
+    Task<ResponseModel<AdDto>> GetAdAsync(Guid id);
+    Task<ResponseModel<GetMainPageDto<AdMainPageDto>>> GetAllAdAsync(int page, string sortdate, string sortprice, string cat);
+    Task<decimal> GetCostAsync(Guid adId, DateTime ebeg, DateTime dend);
+    Task<string> GetOwnerIdAsync(Guid adId);
+
+    #endregion
+    
+    #region Методы адресов
+
+    Task<ResponseModel<Guid>> CreateAddressAsync(T dto);
+    Task<ResponseModel<string>> RemoveAddressAsync(Guid id);
+    Task<ResponseModel<TK?>> GetAddressAsync(Guid id);
+
+    #endregion
+
+    #region Методы категорий
+    Task<ResponseModel<Guid>> CreateCategoryAsync(CreateCategoryDto dto);
+    Task<ResponseModel<CategoryDto?>> GetCategoryAsync(Guid id);
+    Task<ResponseModel<List<CategoryDto?>>> GetAllCategoriesAsync();
+    
+    #endregion
+
+    #region Методы работы с файлами
+
+    Task<ResponseModel<string>> UploadFileAsync(CreateFileDto dto);
+    Task<ResponseModel<string>> RemoveFileAsync(Guid id);
+    Task<ResponseModel<byte[]>> GetFileAsync(Guid id);
+
+    #endregion
+
+    #region Методы поиска
+    Task<List<ResponseModel>> SearchByTitle(string search);
+    #endregion
+
+    #region Методы Тэгов
+    Task<ResponseModel<Guid>> CreateTagAsync(TagDto tag);
+    
+    #endregion
+
+    #region Методы Тарифов
+
+    Task<ResponseModel<Guid>> CreateTarifAsync(CreateTarifDto dto);
+    Task<ResponseModel<bool>> DeleteTarifAsync(Guid id);
+
+    #endregion
+
+    #region Методы Тайм Юнитов
+    Task<ResponseModel<Guid>> CreateAsync(CreateTimeUnitDto dto);
+    Task<ResponseModel<TimeUnitDto?>> GetAsync(Guid id);
+    Task<ResponseModel<List<TimeUnitDto?>>> GetAllAsync();
+
+    #endregion
 }
