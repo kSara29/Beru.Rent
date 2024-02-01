78c8118a7ce7d24a665fe1a29ae00bff4eb9a943
Tyurmina Iolanta <lanax94@mail.ru>
2024-02-01 10:28:12 +0300

    #83 добавила метод SuggestAddress.cs

---
+++ Beru.Rent/Api.Gateway/Bff.Api/Endpoints/AdService/Address/SuggestAddress.cs
@@ -0,0 +1,23 @@
+﻿using Ad.Api.Request;
+using Bff.Application.Contracts;
+using Common;
+using FastEndpoints;
+
+namespace Bff.Api.Endpoints.AdService.Address;
+
+public class SuggestAddress(IAddressService service) : Endpoint<QueryModel, ResponseModel<List<string>>>
+{
+    public override void Configure()
+    {
+        Get("/bff/address/suggestions");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync
+        ([Microsoft.AspNetCore.Mvc.FromBody]QueryModel request,CancellationToken ct)
+    {
+        if (request is null) await SendAsync(null!, cancellation: ct);
+        var response = await service.SuggestAddress(request);
+        await SendAsync(response, cancellation: ct);
+    }
+}
---
+++ Beru.Rent/Api.Gateway/Bff.Application/Services/AdServices/AddressService.cs
@@ -0,0 +1,37 @@
+﻿using Ad.Api.Request;
+using Ad.Application.DTO.CreateDtos;
+using Ad.Application.DTO.GetDtos;
+using Ad.Dto.ResponseDto;
+using Bff.Application.Contracts;
+using Bff.Application.Handlers;
+using Bff.Application.JsonOptions;
+using Common;
+using Microsoft.Extensions.Options;
+
+namespace Bff.Application.Services;
+
+public class AddressService(
+    ServiceHandler serviceHandler,
+    IOptions<RequestToAdApi> jsonOptions):IAddressService
+{
+    public async Task<ResponseModel<GuidResponse>> CreateAddressAsync(CreateAddressExtraDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ResponseModel<StringResponse>> RemoveAddressAsync(RequestById id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ResponseModel<AddressExtraDto?>> GetAddressAsync(RequestById id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ResponseModel<List<string>>> SuggestAddress(QueryModel model)
+    {
+        var url = serviceHandler.CreateConnectionUrlWithoutQuery(jsonOptions.Value.Url, "api/address/suggestions");
+        return await serviceHandler.PostConnectionHandler<QueryModel, List<string>>(url, model);
+    }
+}
