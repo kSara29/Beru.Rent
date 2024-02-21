7df455d550d87a9ad4e1ec024c2213daba87a939
Yevgeniy Agrafenin <eugeniyliudmila@gmail.com>
2024-02-22 00:26:56 +0600

    #92 Устранение неполадок сервера уведомлений

--- Beru.Rent/Services/User.Service/User.Api/Services/EmailService.cs
+++ Beru.Rent/Services/User.Service/User.Api/Services/EmailService.cs
@@ -1,14 +1,27 @@
-﻿namespace User.Api.Services;
+﻿using System.Text;
+using Newtonsoft.Json;
+using Notification.Dto.RequestDto;
+
+namespace User.Api.Services;
 
 public class EmailService(IHttpClientFactory httpClientFactory)
 {
-    private readonly HttpClient _client = httpClientFactory.CreateClient("Notification");
+    
     public async Task SendEmailAsync(string email, string subject, string message)
     {
-        var result = await _client.PostAsJsonAsync("notification/send", message);
+        using var client = httpClientFactory.CreateClient();
+        var dto = new SendMessageRequestDto
+        {
+            Email = email,
+            Subject = subject,
+            Message = message
+        };
+        var jsonContent = JsonConvert.SerializeObject(dto);
+        var contentRequest = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+        var result = await client.PostAsync("notification/send", contentRequest);
         if (result.IsSuccessStatusCode)
         {
-           
+            
         }
         else
         {
