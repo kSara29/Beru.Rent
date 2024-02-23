c10a2a27a86e3ab1377fccb3e72965fbf122d18c
Alexey <mr.alexr96@bk.ru>
2024-02-23 17:38:18 +0600

    #99 Добавлена подсадка 5 юзеров

--- Beru.Rent/Services/User.Service/User.Application/Extencions/Adminitializer.cs
+++ Beru.Rent/Services/User.Service/User.Application/Extencions/Adminitializer.cs
@@ -6,15 +6,60 @@ public class Adminitializer
 {
     public static async Task SeedAdminUserAsync(RoleManager<IdentityRole> roleManager, UserManager<Domain.Models.User> userManager)
     {
-        var admin = new Domain.Models.User
+        var users = new List<Domain.Models.User>
         {
-            Email = "admin@admin.admin",
-            FirstName = "admin",
-            LastName = "admin",
-            Iin = "000000000",
-            UserName = "admin",
-            EmailConfirmed = true
+            new()
+            {
+                Id = "ecab5681-aa11-4f85-b5d5-72dd1705d767",
+                Email = "admin@admin.admin",
+                FirstName = "admin",
+                LastName = "admin",
+                Iin = "000000000",
+                UserName = "admin",
+                EmailConfirmed = true
+            },
+            new()
+            {
+                Id = "f547056b-930a-488d-84bd-70b922138fb9",
+                Email = "Alex@alex.com",
+                FirstName = "Alex",
+                LastName = "Alex",
+                Iin = "000000000",
+                UserName = "Alex",
+                EmailConfirmed = true
+            },
+            new()
+            {
+                Id = "8942be2b-210f-4d49-9331-5c3c47eb9402",
+                Email = "Lana@alex.com",
+                FirstName = "Lana",
+                LastName = "Lana",
+                Iin = "000000000",
+                UserName = "Foxdom",
+                EmailConfirmed = true
+            },
+            new()
+            {
+                Id = "c05263fc-e4f2-44b0-a18f-2bd0bb40aa8f",
+                Email = "Evgeny@evgeny.com",
+                FirstName = "Evgeny",
+                LastName = "Evgeny",
+                Iin = "000000000",
+                UserName = "Evgeny",
+                EmailConfirmed = true
+            },
+            new()
+            {
+                Id = "200aa31e-b533-4e66-99ff-f2c637109e72",
+                Email = "Sara@sara.com",
+                FirstName = "Sara",
+                LastName = "Sara",
+                Iin = "000000000",
+                UserName = "Sara",
+                EmailConfirmed = true
+            }
         };
+        
   
         var roles = new [] { "admin", "user" };
         
@@ -23,12 +68,26 @@ public class Adminitializer
             if (await roleManager.FindByNameAsync(role) is null)
                 await roleManager.CreateAsync(new IdentityRole(role));
         }
-        if (await userManager.FindByNameAsync(admin.Email) == null)
+        
+        foreach (var user in users)
         {
-            var result = await userManager.CreateAsync(admin, "Qwerty123@");
-            if (result.Succeeded)
-                await userManager.AddToRoleAsync(admin, roles[0]);
-        }
+            if (user.UserName == "admin")
+                await CreateUsers(userManager, user, "admin");
 
+            else
+                await CreateUsers(userManager, user, "user");
+        }
+    }
+
+    static async Task CreateUsers
+        (UserManager<Domain.Models.User> userManager, 
+            Domain.Models.User user, string role)
+    {
+        if (await userManager.FindByNameAsync(user.Email) == null)
+        {
+            var result = await userManager.CreateAsync(user, "Qwerty123@");
+            if (result.Succeeded)
+                await userManager.AddToRoleAsync(user, role);
+        }
     }
 }
